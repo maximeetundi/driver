48d613a08f36b44f652aa015204d7d441077c935
Maxime ETUNDI <maximeetundi@gmail.com>
2024-06-28 16:40:09 +0100

    config telescope

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,13 +32,35 @@ class AppServiceProvider extends ServiceProvider
             $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
             $this->app->register(TelescopeServiceProvider::class);
 
-            DB::listen(function ($query) {
+           // tttttttttttttttttttttttttttttttt
+           DB::listen(function ($query) {
+            // Liste des tables Telescope
+            $telescopeTables = [
+                'telescope_entries',
+                'telescope_entries_tags',
+                'telescope_monitoring',
+                // Ajoutez d'autres tables Telescope si nécessaire
+            ];
+
+            // Vérifiez si la requête est liée à Telescope
+            $isTelescopeQuery = false;
+            foreach ($telescopeTables as $table) {
+                if (strpos($query->sql, $table) !== false) {
+                    $isTelescopeQuery = true;
+                    break;
+                }
+            }
+
+            // Si ce n'est pas une requête Telescope, loguez-la
+            if (!$isTelescopeQuery) {
                 Log::info('SQL Query', [
                     'sql' => $query->sql,
                     'bindings' => $query->bindings,
                     'time' => $query->time,
                 ]);
-            });
+            }
+        });
+        //fgggggggggggggggggggggggggggggggggg
         if($this->app->environment('live')) {
             URL::forceScheme('https');
         }
