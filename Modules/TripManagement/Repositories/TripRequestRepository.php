ac552212d227d156884e4d4a06f703179606f5d9
Maxime ETUNDI <maximeetundi@gmail.com>
2024-06-28 15:19:15 +0100

    config telescope

--- Modules/TripManagement/Repositories/TripRequestRepository.php
+++ Modules/TripManagement/Repositories/TripRequestRepository.php
@@ -292,17 +292,17 @@ class TripRequestRepository implements TripRequestInterfaces
     public function getPendingRides($attributes): mixed
     {
         return $this->trip->query()
-//             ->when($attributes['relations'] ?? null, fn($query) => $query->with($attributes['relations']))
-//             ->with([
-//                 'fare_biddings' => fn($query) => $query->where('driver_id', auth()->id()),
-// //                'coordinate' => fn($query) => $query->whereRaw("ST_Distance_Sphere($column, POINT($location->longitude, $location->latitude)) < $distance")
-//                 'coordinate' => fn($query) => $query->distanceSphere('pickup_coordinates', $attributes['driver_locations'], $attributes['distance'])
-//             ])
-            ->whereHas('coordinate',
-                fn($query) => $query->distanceSphere('pickup_coordinates', $attributes['driver_locations'], $attributes['distance']))
-            ->when($attributes['withAvgRelation'] ?? null,
-                fn($query) => $query->withAvg($attributes['withAvgRelation'], $attributes['withAvgColumn']))
-            ->whereDoesntHave('ignoredRequests', fn($query) => $query->where('user_id', auth()->id()))
+            ->when($attributes['relations'] ?? null, fn($query) => $query->with($attributes['relations']))
+            ->with([
+                'fare_biddings' => fn($query) => $query->where('driver_id', auth()->id()),
+//                'coordinate' => fn($query) => $query->whereRaw("ST_Distance_Sphere($column, POINT($location->longitude, $location->latitude)) < $distance")
+                'coordinate' => fn($query) => $query->distanceSphere('pickup_coordinates', $attributes['driver_locations'], $attributes['distance'])
+            ])
+//             ->whereHas('coordinate',
+//                 fn($query) => $query->distanceSphere('pickup_coordinates', $attributes['driver_locations'], $attributes['distance']))
+//             ->when($attributes['withAvgRelation'] ?? null,
+//                 fn($query) => $query->withAvg($attributes['withAvgRelation'], $attributes['withAvgColumn']))
+//             ->whereDoesntHave('ignoredRequests', fn($query) => $query->where('user_id', auth()->id()))
             ->where(fn($query) =>
                 $query->where('vehicle_category_id', $attributes['vehicle_category_id'])
                     ->orWhereNull('vehicle_category_id')
